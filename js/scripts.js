f1915fc0fb939d14901e3909e43dee7c88a2ef74
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-02-14 09:49:55 -0300

    select user and message type

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 let username = {name: ''};
 
 function sendUsername() {
-    username.name = prompt('Digite o nome do usuário:');
+    username.name = document.querySelector('.login-screen input').value;
     
     const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', username);
     promise.then(() => {
+        document.querySelector('.login-screen').classList.add('hidden');
         getMessages();
+        getParticipants();
+
         stayOnline();
         setInterval(getMessages, 3000);
+        setInterval(getParticipants, 10000);
     });
     promise.catch((error) => {
         if (error.response.status === 400) {
             alert('Esse nome não está disponível');
-            sendUsername();
         } else {
             alert('Erro desconhecido');
             window.location.reload();
         }
-    })
+    });
 }
 
 function stayOnline() {
@@ -65,7 +68,7 @@ function displayChat(messages) {
             `;
 
             document.querySelector('.message:last-child').scrollIntoView();
-        } else if (message.type === 'private_message' && message.to === username) {
+        } else if (message.type === 'private_message' && (message.to === username.name || message.from === username.name)) {
             chat.innerHTML += `
                 <li class="message private" data-identifier="message">
                     <p><span>(${message.time})</span> <strong>${message.from}</strong> reservadamente para <strong>${message.to}</strong> ${message.text}</p>
@@ -79,22 +82,68 @@ function displayChat(messages) {
 
 function sendMessage() {
     const messageText = document.querySelector('footer input').value;
+    const messageTo = document.querySelector('footer span:first-of-type').innerHTML;
+    const config = document.querySelector('footer span:last-child').innerHTML;
+    let messageType = null;
+    if (config === 'publicamente') {
+        messageType = 'message';
+    } else if (config === 'reservadamente') {
+        messageType = 'private_message';
+    }
+
     const message = {
         from: username.name,
-        to: "Todos",
+        to: messageTo,
         text: messageText,
-        type: "message"
+        type: messageType
     };
 
     const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', message);
     promise.then((response) => {
+        console.log(message);
         console.log(response.data);
         getMessages();
+        document.querySelector('footer input').value = '';
     });
     promise.catch((error) => {
         console.log(error.response);
         window.location.reload();
     });
+}
+
+function getParticipants() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants');
+    promise.then((response) => {
+        displayParticipants(response.data);
+    });
+    promise.catch((error) => {
+        console.log(error.response);
+    });
+}
+
+function displayParticipants(participants) {
+    const userList = document.querySelector('.contacts');
+    userList.innerHTML = `
+        <li onclick="selectContact(this)">
+            <div>
+                <ion-icon name="people"></ion-icon>
+                <p>Todos</p>
+            </div>
+            <img src="images/check.svg" alt="checkmark">
+        </li>
+    `;
+
+    for (let i=0; i<participants.length; i++) {
+        userList.innerHTML += `
+        <li onclick="selectContact(this)">
+            <div>
+                <ion-icon name="person-circle"></ion-icon>
+                <p>${participants[i].name}</p>
+            </div>
+            <img src="images/check.svg" alt="checkmark" class="hidden">
+        </li>
+    `;
+    }
 }
 
 function toggleMenu() {
@@ -105,4 +154,25 @@ function toggleMenu() {
     background.classList.toggle('hidden')
 }
 
-//sendUsername();
+function selectContact(user) {
+    const checkmarked = document.querySelector('.contacts img:not(.hidden)');
+    if (!!checkmarked) checkmarked.classList.add('hidden');
+
+    user.querySelector('img').classList.remove('hidden');
+    const name = user.querySelector('p').innerHTML;
+    document.querySelector('footer span:first-child').innerHTML = name;
+}
+
+function selectVisibility(item) {
+    const checkmarked = document.querySelector('.visibility img:not(.hidden)');
+    if (!!checkmarked) checkmarked.classList.add('hidden');
+
+    item.querySelector('img').classList.remove('hidden');
+    const config = item.querySelector('p').innerHTML;
+
+    if (config === 'Público') {
+        document.querySelector('footer span:last-child').innerHTML = 'publicamente';
+    } else if (config === 'Reservadamente') {
+        document.querySelector('footer span:last-child').innerHTML = 'reservadamente';
+    }
+}
